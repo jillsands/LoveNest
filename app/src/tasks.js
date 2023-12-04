9b1bdcf3e6b3f95dff9db81ac7f3f25785397033
Jillian Sands <jesands@umd.edu>
2023-12-04 02:11:47 -0500

    update tasks

--- app/src/tasks.js
+++ app/src/tasks.js
@@ -1,9 +1,193 @@
 import { COLUMN_NAMES } from './constants';
 
-const { UNSORTED } = COLUMN_NAMES;
+const { UNSORTED, PRTNR_1 } = COLUMN_NAMES;
 export const tasks = [
-  { name: 'Item 1', column: UNSORTED },
-  { name: 'Item 2', column: UNSORTED },
-  { name: 'Item 3', column: UNSORTED },
-  { name: 'Item 4', column: UNSORTED },
+  {
+    name: 'Cleaning',
+    includes: 'vacuuming, wiping, dusting, sanitizing, etc.',
+    column: PRTNR_1,
+  },
+  {
+    name: 'Dishes',
+    includes: 'washing, drying, loading, unloading',
+    isDaily: true,
+    column: PRTNR_1,
+  },
+  {
+    name: 'Trash',
+    includes: 'taking to the curb/shoot, replacing bags',
+    column: PRTNR_1,
+  },
+  {
+    name: 'Groceries',
+    includes: 'keeping a list, shopping for food',
+    isDaily: true,
+    column: PRTNR_1,
+  },
+  {
+    name: 'Home Supplies',
+    includes: 'keeping a list, shopping for detergent, tootpaste, etc.',
+    isDaily: true,
+    column: PRTNR_1,
+  },
+  {
+    name: 'Home Furnishings',
+    includes: 'keeping a list, shopping for pillows, towels, etc.',
+    isDaily: true,
+    column: PRTNR_1,
+  },
+  {
+    name: 'Maintenance',
+    includes: 'unclogging toilets, calling handyman, etc.',
+    isDaily: true,
+    column: PRTNR_1,
+  },
+  {
+    name: 'Home Purchase/Rental',
+    includes: 'insurance, reviewing options/statements',
+    column: PRTNR_1,
+  },
+  {
+    name: 'Hosting',
+    includes: 'sending invites, planning menu, decorating',
+    column: PRTNR_1,
+  },
+  {
+    name: 'Laundry',
+    includes: 'washing/drying, folding, storing',
+    isDaily: true,
+    column: PRTNR_1,
+  },
+  {
+    name: 'Lawn & Plants',
+    includes: 'mow, water, landscape',
+    column: PRTNR_1,
+  },
+  {
+    name: 'Mail',
+    includes: 'retrieving, opening, and sending',
+    isDaily: true,
+    column: PRTNR_1,
+  },
+  {
+    name: 'Breakfast',
+    includes: 'plan menu, cook',
+    isDaily: true,
+    column: PRTNR_1,
+  },
+  {
+    name: 'Dinner',
+    includes: 'plan menu, cook',
+    isDaily: true,
+    column: PRTNR_1,
+  },
+  {
+    name: 'Money Management',
+    includes: 'budgeting, long-term planning, taxes',
+    column: PRTNR_1,
+  },
+  { name: 'Storage, Garage, & Donations', column: PRTNR_1 },
+  {
+    name: 'Tidying',
+    includes: 'making bed, clearing countertops and floors',
+    isDaily: true,
+    column: PRTNR_1,
+  },
+  {
+    name: 'Auto',
+    includes: 'registration, insurance, maintenance, gas',
+    column: PRTNR_1,
+  },
+  {
+    name: 'Calendar',
+    includes: 'scheduling and maintaining',
+    isDaily: true,
+    column: PRTNR_1,
+  },
+  { name: 'Paying Bills', isDaily: true, column: PRTNR_1 },
+  {
+    name: 'Charity & Community Service',
+    includes: 'giving back time and money',
+    column: PRTNR_1,
+  },
+  {
+    name: 'Civic Engagement & Cultural Enrichment',
+    includes: 'research and plan community events',
+    column: PRTNR_1,
+  },
+  {
+    name: 'Tech',
+    includes: 'fixing/getting help for issues, buying new items',
+    column: PRTNR_1,
+  },
+  {
+    name: 'First-Aid, Safety & Emergency',
+    includes: 'stocking supplies, making plans',
+    column: PRTNR_1,
+  },
+  { name: 'Points, Miles, & Coupons', column: PRTNR_1 },
+  { name: 'Returns & Store Credit', column: PRTNR_1 },
+  {
+    name: 'Social Plans',
+    includes: 'syncing calendars, picking time and place, invites',
+    column: PRTNR_1,
+  },
+  { name: 'Vacations & Trips', includes: '', column: PRTNR_1 },
+  {
+    name: 'Grooming & Wardrobe (Partner 1)',
+    includes: 'appointments, shopping',
+    column: PRTNR_1,
+  },
+  {
+    name: 'Grooming & Wardrobe (Partner 2)',
+    includes: 'appointments, shopping',
+    column: PRTNR_1,
+  },
+  {
+    name: 'Estate Planning & Life Insurance',
+    includes: 'planning and arranging',
+    column: PRTNR_1,
+  },
+  {
+    name: 'Health Insurance',
+    includes: 'paperwork, tracking reimbursements',
+    column: PRTNR_1,
+  },
+  {
+    name: 'Parents',
+    includes: 'facilitating conversations and meet-ups',
+    column: PRTNR_1,
+  },
+  {
+    name: 'Pet',
+    includes: 'food, vet, supplies, grooming, walks/cleaning',
+    isDaily: true,
+    column: PRTNR_1,
+  },
+  {
+    name: 'Extended Family',
+    includes: 'reconnecting, remembering invites',
+    column: PRTNR_1,
+  },
+  { name: 'Fun & Dates', includes: 'scheduling and planning', column: PRTNR_1 },
+  {
+    name: 'Gifts',
+    includes: 'meaningful gifts and cards for others',
+    column: PRTNR_1,
+  },
+  {
+    name: 'Holidays',
+    includes: 'cooking, decor, organizing',
+    column: PRTNR_1,
+  },
+  {
+    name: 'Spirituality',
+    includes: 'planning attendance, rituals, and events, enforcing beliefs',
+    column: PRTNR_1,
+  },
+  {
+    name: 'Medicine & Healthy Living',
+    includes: 'research, stocking health-related items',
+    column: PRTNR_1,
+  },
 ];
